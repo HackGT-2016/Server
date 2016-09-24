e4095f4ec57f59c35d0a4af3d1508e7bf4532265
alukin3 <luk.andrey@gatech.edu>
2016-09-24 12:30:05 -0400

    Set up different pages for the voting 'app'

--- Strive/voting/models.py
+++ Strive/voting/models.py
@@ -4,16 +4,26 @@ from django.db import models
 
 
 class User(models.Model):
+    username = models.CharField(max_length=200)
     email = models.CharField(max_length = 200)
     password = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return self.username;
 
 class Company(models.Model):
     name = models.CharField(max_length = 300)
     description = models.CharField(max_length = 300)
     prizes = models.CharField(max_length = 300)
 
+    def __str__(self):
+        return self.name;
+
 class Tournament(models.Model):
     time_created = models.DateTimeField('date started')
+
+    def __str__(self):
+        return self.username;
 
 class Match(models.Model):
     id_matchup = models.IntegerField(default = 0)
@@ -22,8 +32,14 @@ class Match(models.Model):
     tournament = models.ForeignKey(Tournament)
     a_won_true = models.BooleanField()
 
+    def __str__(self):
+        return str(self.id_matchup) + " " + this.company_match_a + " : " + this.company_match_b;
+
 class Vote(models.Model):
     amount = models.IntegerField(default = 0)
     username = models.ForeignKey(User)
     company_match = models.ForeignKey(Match)
+
+    def __str__(self):
+        return this.company_match + " : " + this.username;
 
